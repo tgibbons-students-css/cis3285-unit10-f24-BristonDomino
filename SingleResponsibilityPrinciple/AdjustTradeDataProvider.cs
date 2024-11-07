6f8c53bb3bde36af7e782a9a632c3f03e83a0ea5
Briston Domino <Bdomino1@css.edu>
2024-11-07 13:32:35 -0600

    NotDoneMovingcokputers

---
+++ SingleResponsibilityPrinciple/AdjustTradeDataProvider.cs
@@ -0,0 +1,35 @@
+﻿using SingleResponsibilityPrinciple.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class AdjustTradeDataProvider : ITradeDataProvider
+    {
+        private readonly ITradeDataProvider origProvider;
+
+        public AdjustTradeDataProvider(ITradeDataProvider origProvider)
+        {
+            this.origProvider = origProvider;
+        }
+
+        public IEnumerable<string> GetTradeData()
+        {
+            // call orignal GetTradeData
+            IEnumerable<string> lines = origProvider.GetTradeData();
+
+            List<string> result = new List<string>();
+            foreach (String line in lines)
+            {
+                String newLine = line.Replace("GBP", "EUR");
+                result.Add(newLine);
+            }
+
+            return lines;
+        }
+    }
+
+}
